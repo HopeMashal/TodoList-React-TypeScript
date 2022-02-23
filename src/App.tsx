1beec34d613fe8ac2320ac38016396772bba54b5
HopeMashal <mashal.hope@gmail.com>
2022-02-23 15:49:05 +0200

    edit fews files

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import InputField from './components/InputField';
+import { Todo } from './models/models';
 
 const App: React.FC = () => {
+
+  const [todo, setTodo] = useState<string>("");
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const handleAdd = () => {};
+
   return (
     <div className="App">
       <div className='Heading'>
         <img src="./logo.png" alt="Logo" className='Logo'/>
         <span className="Header">Hope ToDo List</span>
       </div>
-      <InputField/>
+      <InputField todo={todo} setTodo={setTodo}/>
     </div>
   );
 }
--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import './styles.css'
 
-const InputField = () => {
+interface Props{
+  todo:string;
+  setTodo:React.Dispatch<React.SetStateAction<string>>;
+}
+
+const InputField = ({todo, setTodo} : Props) => {
   return (
     <form className='InputField'>
-      <input type="input" placeholder='Enter a new task' className='Input'/>
+      <input type="input" placeholder='Enter a new task' value={todo} onChange={(e)=> setTodo(e.target.value)} className='Input'/>
       <button className='SubmitBtn' type='submit'>Add</button>
     </form>
   )
