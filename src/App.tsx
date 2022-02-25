1eab7b33f7b31ddbb8b245f83f379d2a41f507ac
HopeMashal <mashal.hope@gmail.com>
2022-02-25 14:51:14 +0200

    edit

--- src/App.tsx
+++ src/App.tsx
@@ -58,6 +58,12 @@ const App: React.FC = () => {
     }
     setCompletedTodos(complete)
     setTodos(active)
+    setCompletedTodos(
+      complete.map((todo)=>
+        todo.isDone===false?{...todo,isDone:!todo.isDone}:todo))
+    setTodos(
+      active.map((todo)=>
+        todo.isDone===true?{...todo,isDone:!todo.isDone}:todo))
     localStorage.setItem("completedtodos", JSON.stringify(completedTodos))
     localStorage.setItem("todos", JSON.stringify(todos))
   }
@@ -77,62 +83,3 @@ const App: React.FC = () => {
 }
 
 export default App;
-
-/* 
-import React, { useState } from 'react';
-import './App.css';
-import InputField from './components/InputField';
-import TodoList from './components/TodoList';
-import { Todo } from './models/models';
-import {DragDropContext, DropResult} from 'react-beautiful-dnd'
-
-const App: React.FC = () => {
-
-  const [todo, setTodo] = useState<string>("");
-  const [todos, setTodos] = useState<Todo[]>([]);
-  const [completedTodos, setCompletedTodos] = useState<Todo[]>([]);
-
-  const handleAdd = (e : React.FormEvent) => {
-    e.preventDefault();
-    if(todo){
-      setTodos([...todos,{id : Date.now(), todo, isDone:false}])
-      setTodo("");
-    }
-  };
-
-  const onDragEnd =(result:DropResult)=>{
-    const {source,destination}=result;
-    if(!destination)return;
-    if(destination.droppableId===source.droppableId && destination.index===source.index) return;
-    let add, active = todos, complete = completedTodos;
-    if(source.droppableId === "TodosList"){
-      add = active[source.index];
-      active.splice(source.index, 1)
-    } else {
-      add = complete[source.index];
-      complete.splice(source.index , 1)
-    }
-    if(destination.droppableId === "TodosList"){
-      active.splice(destination.index, 0 , add)
-    } else {
-      complete.splice(destination.index , 0 , add)
-    }
-    setCompletedTodos(complete)
-    setTodos(active)
-  }
-
-  return (
-    <DragDropContext onDragEnd={onDragEnd}>
-      <div className="App">
-        <div className='Heading'>
-          <img src="./logo.png" alt="Logo" className='Logo'/>
-          <span className="Header">Hope ToDo List</span>
-        </div>
-        <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd}/>
-        <TodoList todos={todos} setTodos={setTodos} completedTodos={completedTodos} setCompletedTodos={setCompletedTodos}/>
-      </div>
-    </DragDropContext>
-  );
-}
-
-export default App; */
